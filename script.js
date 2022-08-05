727ca9093a4622e88e111850e14371e97c91d458
Lucas Rivera <lucasrivera89@gmail.com>
2022-08-04 21:28:51 -0300

    primeraEntregaFinal

--- script.js
+++ script.js
@@ -4,18 +4,24 @@ const errores = []
 let respuesta
 let vidas = 5
 
-
+alert("Bienvenido a Narutole. Para salir en cualquier momento ingrese ESC")
 
 for(vidas = 4; vidas >= 0;vidas--){
+  
   console.log(personaje)
   respuesta = prompt("Ingrese el nombre de un personaje").toLowerCase()
   errores.push(respuesta)
   console.log(respuesta)
 
-
-
+  
+  if(respuesta == "esc"){
+    alert("gracias por participar.")
+    break;
+  }
+ 
   if(respuesta == personaje){
     alert(`Felicitaciones, la respuesta era ${personaje}`)
+    break;
   } else {
     alert(`Te quedan ${vidas} vidas`)
     console.log(errores)
